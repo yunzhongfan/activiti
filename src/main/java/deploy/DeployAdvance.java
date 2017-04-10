153ceff6697b3b336b809d474a89e03159a9b1b2
yunzhongfan <763748729@qq.com>
2017-04-10 21:11:38 +0800

    wqeeqewqe

---
+++ src/main/java/deploy/DeployAdvance.java
@@ -0,0 +1,15 @@
+package deploy;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+
+/**
+ *流程定义 高级操作，进阶篇
+ * @author nbc
+ *
+ */
+
+public class DeployAdvance { 
+	ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+	
+}
